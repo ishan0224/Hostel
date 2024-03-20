00cb8bbc756f4b26455ecd12f0c53e9dc58c9429
github_ishan0224 <sinha2ishan4@gmail.com>
2024-03-20 22:07:31 +0530

    Second commit Added password hide show to login pages

--- Tail/public/userLogin.js
+++ Tail/public/userLogin.js
@@ -1,7 +1,27 @@
-let subBtn = document.getElementById("submitBtn");
+const subBtn = document.getElementById("submit-btn");
 
-addEventListener("click", (event)=>{
+const emailField = document.getElementById("email");
+const passField = document.getElementById("password");
+
+  
+subBtn.addEventListener("click", (event) => {
+
+    
+
     event.preventDefault();
     event.stopPropagation();
+    if (emailField.value != "" && passField.value != "") {
     location.href = "userForm.html"
+    }
+    else{
+        alert("Please enter your credentials");
+    }
 })
+const togglePassword = document.getElementById('togglePassword');
+const password = document.getElementById('password');
+
+togglePassword.addEventListener('click', function () {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('text-gray-400');
+});
